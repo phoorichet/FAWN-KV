d71c11dbe932d801d710efea4860f627da4c50bd
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2013-04-28 16:07:49 -0400

    add silt.thrift

---
+++ siltds/silt.thrift
@@ -0,0 +1,15 @@
+namespace cpp silt
+
+service SiltCluster {
+  
+  i32 join(1:string ip, 2:i32 port),
+  i32 put(1:string key, 2:string value),
+  string get(1:string key)
+
+}
+
+service SiltNode {
+  i32 connect_master(1:string ip, 2:i32 port),
+  i32 put(1:string key, 2:string value),
+  string get(1:string key)
+}
