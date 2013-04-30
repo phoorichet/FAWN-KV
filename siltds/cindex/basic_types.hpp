19a577de6c9b7d0b289d1b999150253b466909ae
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2013-04-30 19:42:03 -0400

    use static conversion

--- siltds/cindex/basic_types.hpp
+++ siltds/cindex/basic_types.hpp
@@ -13,14 +13,14 @@ namespace cindex
 	template<typename T, typename S>
 	inline T guarded_cast(const S& v)
 	{
-    // cout << "converting " << v << "->" << static_cast<T>(v) << endl;
-#ifndef NDEBUG
-//     T ret = boost::numeric::converter<T, S>::convert(v); 
-//     cout<< "++++++++++++0000+++++++++++++++"<< ret <<endl;
-		return boost::numeric::converter<T, S>::convert(v); 
-#else
+    cout << "converting " << v << "->" << static_cast<T>(v) << endl;
+// #ifndef NDEBUG
+// //     T ret = boost::numeric::converter<T, S>::convert(v); 
+// //     cout<< "++++++++++++0000+++++++++++++++"<< ret <<endl;
+// 		return boost::numeric::converter<T, S>::convert(v); 
+// #else
 		return static_cast<T>(v);
-#endif
+// #endif
 
     
 	}
